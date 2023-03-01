2a423084fe127b2d4aba90b644e8208c2aa30cc1
Raul <raulmayas20@gmail.com>
2023-02-28 20:53:52 -0600

    Posting title and text immediately

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 // Importing my JSON File
 const dbTest = require("./db/db.json");
 
@@ -24,9 +25,41 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
 // GET request
 app.get("/api/notes", (req, res) => {
-  return res.json(dbTest);
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
 
 // POST request
@@ -42,44 +75,13 @@ app.post("/api/notes", (req, res) => {
     // Variable for the object we will save
     const newReview = {
       title,
-      text
+      text,
     };
 
-    // Write the string to a file
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-
-      // 3. Parse the existing JSON data into an object
-      const existingData = JSON.parse(data);
-      console.log(typeof existingData, existingData);
-
-      // 4. Append new data to the existing JSON object
-      existingData.push(newReview);
-      console.log(typeof existingData, existingData);
-      // 5. Convert the updated JSON object back to a string
-      const updatedData = JSON.stringify(existingData, null, 2);
-
-      console.log(typeof updatedData, updatedData);
-      // 6. Write the updated JSON string back to the file
-      // Write the string to a file
-      fs.writeFile(`./db/db.json`, updatedData, (err) =>
-        err
-          ? console.error(err)
-          : console.log(
-              `Review for ${newReview.product} has been written to JSON file`
-            )
-      );
-    });
-
-    const response = {
-      status: "success",
-      body: newReview,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
+    readAndAppend(newReview, "./db/db.json");
+    res.json(`Note added successfully 🚀`);
   } else {
-    res.status(500).json("Error in posting review");
+    res.error("Error in adding note");
   }
 });
 
